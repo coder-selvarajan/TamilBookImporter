41f959829d71f503323f39a0556007d49cead5ea
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-06-22 19:02:49 +0530

    Initial Commit

---
+++ TamilBookImporter/TamilBookImporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamilBookImporterApp.swift
+//  TamilBookImporter
+//
+//  Created by Selvarajan on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TamilBookImporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
